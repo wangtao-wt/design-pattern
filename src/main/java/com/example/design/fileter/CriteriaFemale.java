b8c4e7c5737b36f036db4aa481d7612f4714a663
v-tao.wang <v-tao.wang@wz-inc.com>
2020-05-29 09:42:09 +0800

    add filter pattern

---
+++ src/main/java/com/example/design/fileter/CriteriaFemale.java
@@ -0,0 +1,17 @@
+package com.example.design.fileter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CriteriaFemale implements Criteria {
+    @Override
+    public List<Person> meetCriteria(List<Person> persons) {
+        ArrayList<Person> femalePersons = new ArrayList<>();
+        for (Person person : persons) {
+            if (person.getGender().equalsIgnoreCase("FEMALE")) {
+                femalePersons.add(person);
+            }
+        }
+        return femalePersons;
+    }
+}
